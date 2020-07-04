84abbb119795a71015a2b4c447023b0e8945dec7
579673 <579673@stud.hvl.no>
2020-07-04 23:15:12 +0200

    Updated BlogForm to correctly add new posts to the db

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useInput } from '../hooks/input-hook';
 import database from '../database';
 import moment from 'moment';
 
-function NameForm(props) {
+function BlogForm(props) {
     const { value: title, bind: bindTitle, reset: resetTitle } = useInput('');
     const { value: body, bind: bindBody, reset: resetBody } = useInput('');
 
-    const handleSubmit = evt => {
+    const handleSubmit = async evt => {
         evt.preventDefault();
 
         const date = moment().toISOString();
         const weekNumber = moment().week();
-        const yearWeek = `${moment().year()}-${moment().week()}`;
+        const year = moment().year();
+        const yearWeek = `${year}-${weekNumber}`;
 
         console.log(title, body, date, weekNumber);
         console.log(moment(date));
 
-        database.ref('weeks').push(yearWeek).set({
+        //TODO: Update form to work with new schema
+        // Check if current week is in db
+        const weekExists = await database
+        .ref(`weeks/${yearWeek}`);
+        
+        // if not, create week
+        if (!weekExists) {
+            database
+            .ref(`weeks/${yearWeek}`)
+            .set({
+                posts: {},
+                weekNumber,
+                year
+            });
+        }
+        // add new post to week
+        database
+        .ref(`weeks/${yearWeek}/posts`)
+        .push()
+        .set({
             title,
             body,
             date,
@@ -48,4 +68,4 @@ function NameForm(props) {
     );
 }
 
-export default NameForm;
+export default BlogForm;
