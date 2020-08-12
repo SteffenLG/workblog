850667209365206b131679fe4fb7086650fca679
Steffen Lid Gaustad <579673@stud.hvl.no>
2020-08-12 10:28:52 +0200

    Some CSS and Firebase auth

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./App.css";
 import Blog from "./components/Blog";
 import BlogForm from "./components/BlogForm";
+import firebaseui from "firebaseui";
 
 function App() {
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start("#firebaseui-auth-container", {
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  });
+
   return (
     <div className="App">
       <header className="App-header">
